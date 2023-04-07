8b60d0fd9062b939ee03d9358e6fe72250a68f2d
Abebe-Nigusu <abebe.nigusu@yahoo.com>
2023-04-07 16:25:18 -0500

    CareSoft

---
+++ src/com/caresoft/clinicapp/User.java
@@ -0,0 +1,39 @@
+package com.caresoft.clinicapp;
+
+public class User {
+
+	
+	protected Integer id;
+	protected int pin;
+	
+	public User() {
+		super();
+	}
+
+	public User(Integer id, int pin) {
+		super();
+		this.id = id;
+		this.pin = pin;
+	}
+
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public int getPin() {
+		return pin;
+	}
+
+	public void setPin(int pin) {
+		this.pin = pin;
+	}
+
+	// TO DO: Getters and setters
+	// Implement a constructor that takes an ID
+
+}
